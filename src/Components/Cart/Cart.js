e01a89b4d86647a3d00acdbc13019cf969dfed6c
Esau Matias <esaufreirematias@gmail.com>
2022-03-24 15:46:59 -0300

    feat: (cart) - implementa função de aumentar e diminuir quantidade do produto no carrinho de compras

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,24 +1,39 @@
 import React, { useContext } from 'react';
 import AppContext from '../../Contex/AppContex';
+import { Button } from 'react-bootstrap';
 import './Cart.css';
 
 function Cart() {
   const { carItens, setCarItens, priceCart, setPriceCart } = useContext(AppContext);
 
-  function sumQuanty(idx) {
-    const quantity = carItens.filter((value) => value.index === idx)
-    return quantity.length;
-  }
-
-  function deleteIten(index, price) {
-    const newArray = carItens.filter((value) => value.index !== index);
-    setPriceCart(priceCart - (price * sumQuanty(index)))
-    setCarItens(newArray);
-  }
-
   const itensCartNew = carItens.filter(function (a) {
     return !this[JSON.stringify(a)] && (this[JSON.stringify(a)] = true);
   }, Object.create(null))
+  
+
+  function sumQuanty(name) {
+    const quantity = carItens.filter((value) => value.name === name)
+    return quantity.length;
+  }
+
+  function removeIten(price, name) {
+    const itenCart = carItens.slice();
+    const index = carItens.findIndex((item) => item.name === name);
+    itenCart.splice(index, 1);
+    setPriceCart(priceCart - price)
+    setCarItens(itenCart);
+  }
+
+  function addItenCart(value) {
+    setCarItens([...carItens, value]);
+    setPriceCart(priceCart + value.price)
+  }
+
+  // function deleteIten(name, price) {
+  //   const newArray = carItens.filter((value) => value.name !== name);
+  //   setPriceCart(priceCart - (price * sumQuanty(name)))
+  //   setCarItens(newArray);
+  // }
 
   return (
     <div className="containerCart">
@@ -26,12 +41,14 @@ function Cart() {
 
       <div className="containerItensCart">
         {itensCartNew.map((value, index) => (
-          <div style={{display: 'flex', alignItems: 'center' }} key={index} className={index % 2 === 0 ? 'par' : 'impar'} onClick={() => deleteIten(value.index, value.price)}>
+          <div style={{display: 'flex', alignItems: 'center' }} key={index} className={index % 2 === 0 ? 'par' : 'impar'} >
             <img src={value.img} alt={value.name} style={{width: '50px', height: '50px', borderRadius: '50%', marginLeft: '3px', marginRight: '5px' }}/>
             <div>
               <p>{value.name}</p>
-              <p>{`QUANTIDADE: ${(sumQuanty(value.index))}`}</p>
-              <strong>{`R$ ${(value.price * sumQuanty(value.index)).toFixed(2)}`}</strong>
+              <div>
+                <Button onClick={() => removeIten(value.price, value.name)}>-</Button>{(sumQuanty(value.name))}<Button onClick={() => addItenCart(value)}>+</Button>
+              </div>
+              <strong>{`R$ ${(value.price * sumQuanty(value.name)).toFixed(2)}`}</strong>
             </div>
           </div>
         ))}
